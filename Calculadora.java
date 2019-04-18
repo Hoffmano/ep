e23744cf41bbc30b27fae6807d3ec3c0a1cfc2f0
hoffman <trotogamer@gmail.com>
2019-04-18 13:19:15 -0300

    soma finalizada com overflow e complemento de 2

--- Calculadora.java
+++ Calculadora.java
@@ -2,11 +2,16 @@ import java.util.Arrays;
 
 class Calculadora {
 
+    private static int carry;
     Dados dados = new Dados();
     private int resultado;
 
     private static void overflowDetection(int[] a, int[] b, int[] c) {
         if ((a[0] == 0 && b[0] == 0 && c[0] == 1) || (a[0] == 1 && b[0] == 1 && c[0] == 0)) {
+            System.out.println("OVERFLOW DETECTED");
+            Runtime.getRuntime().exit(1);
+        }
+        else if(Calculadora.carry == 1){
             System.out.println("OVERFLOW DETECTED");
             Runtime.getRuntime().exit(1);
         }
@@ -17,7 +22,16 @@ class Calculadora {
 
         switch (dados.getOperacao()) {
         case 0:
+            
             int[] res = soma(Dados.getIntA(), Dados.getIntB());
+
+            if(Dados.getOldA().length == 0){
+                Dados.setOldA(Dados.getaBinary());
+            }
+            if(Dados.getOldB().length == 0){
+                Dados.setOldB(Dados.getbBinary());
+            }
+
             Calculadora.overflowDetection(Dados.getOldA(), Dados.getOldB(), res);
             System.out.println(arrayString(res));
 
@@ -58,9 +72,15 @@ class Calculadora {
         // codigo retirado da internet
         // https://www.w3resource.com/java-exercises/basic/java-basic-exercise-17.php
 
+
+        //http://teaching.idallen.com/dat2343/10f/notes/040_overflow.txt
+   
+       
         int[] sum = new int[BinaryConvert.getNumBits()];
         int remainder = 0;
         int i = BinaryConvert.getNumBits() - 1;
+        
+        
 
         while (binary1 != 0 || binary2 != 0) {
             sum[i--] = (int) ((binary1 % 10 + binary2 % 10 + remainder) % 2);
@@ -72,13 +92,11 @@ class Calculadora {
          if (remainder != 0) { 
             if (i >= 0) {
                 sum[i] = remainder; 
-            } 
+            }
             else{
-                //System.out.println("ERRO: OVERFLOW");
-                //Runtime.getRuntime().exit(1); 
-            } 
-        }
-         
+                //Calculadora.carry = remainder;
+            }
+        }         
 
         return sum;
 
